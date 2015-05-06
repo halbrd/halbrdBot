91884b4c3f34995e561703e691c6b8f355d1ca46
halbrd <halbrd@gmail.com>
2015-05-06 12:36:37 +1000

    Upload urlget.py (Github sucks)

---
+++ urlget.py
@@ -0,0 +1,24 @@
+import urllib2
+import urllib
+import urlparse
+import requests
+import json
+from collections import namedtuple
+from BeautifulSoup import BeautifulSoup
+
+
+
+def urlget(url):
+    if "youtube.com" in url:
+        url_data = urlparse.urlparse(url)
+        query = urlparse.parse_qs(url_data.query)
+        video = query["v"][0] # video_id
+        new_url = "https://gdata.youtube.com/feeds/api/videos/" + video +  "?v=2&alt=jsonc"
+        response = urllib.urlopen(new_url)
+        data = json.loads(response.read())
+        return "[Title] " + data['data']['title'] + " [" + str(int(data['data']['duration'])/60) + "." + str(int(data['data']['duration']) % 60) + " minutes]"
+    else:
+        resp = requests.get(url)
+        data = resp.text
+        soup = BeautifulSoup(data)
+        return "[Title] " + soup.title.string
