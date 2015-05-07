77e5cbc983e92560f969fafa3cc8a263a45e7fcb
Hugo <hugomd@users.noreply.github.com>
2015-05-07 18:56:12 +1000

    Hack_the_Gibson2.0

--- urlget.py
+++ urlget.py
@@ -1,7 +1,9 @@
+import urllib2
 import urllib
 import urlparse
 import requests
 import json
+from collections import namedtuple
 from BeautifulSoup import BeautifulSoup
 
 
@@ -10,11 +12,16 @@ def urlget(url):
     if "youtube.com" in url:
         url_data = urlparse.urlparse(url)
         query = urlparse.parse_qs(url_data.query)
-        video = query["v"][0] # video_id
-        new_url = "https://gdata.youtube.com/feeds/api/videos/" + video +  "?v=2&alt=jsonc"
+        video = query["v"][0]
+        new_url = "https://www.googleapis.com/youtube/v3/videos?id=" + video + "&key=AIzaSyD860e0YpYvAtiFGbLavC4_tyhhpAHTq_M&part=contentDetails,snippet"
         response = urllib.urlopen(new_url)
         data = json.loads(response.read())
-        return "[Title] " + data['data']['title'] + " [" + str(int(data['data']['duration'])/60) + "." + str(int(data['data']['duration']) % 60) + " minutes]"
+        title = data['items'][0]['snippet']['title']
+        description = data['items'][0]['snippet']['description'][:100] + "..."
+        duration = data['items'][0]['contentDetails']['duration']
+        duration = duration[2:-1].split("M")
+        
+        return "[Title] " + title + " [" + duration[0] + " min " + duration[1] + "s]\n[Description] " + description
     else:
         resp = requests.get(url)
         data = resp.text
